cba535452e27ebc8b0d9e4ee29666f2dd5145b58
Henry Whitaker <36062479+henrywhitaker3@users.noreply.github.com>
2025-02-07 21:16:02 +0000

    feat: add option to specify postgres ca cert file (#168)

--- internal/config/config.go
+++ internal/config/config.go
@@ -28,42 +28,43 @@ func (l LogLevel) Level() zap.AtomicLevel {
 }
 
 type Postgres struct {
-	Url string `yaml:"url" env:"URL, overwrite"`
+	Url        string `yaml:"url"          env:"URL, overwrite"`
+	CaCertFile string `yaml:"ca_cert_file" env:"CA_CERT_FILE, overwrite"`
 }
 
 type Redis struct {
-	Addr          string        `yaml:"addr" env:"ADDR, overwrite"`
-	Password      string        `yaml:"password" env:"PASSWORD, overwrite"`
+	Addr          string        `yaml:"addr"            env:"ADDR, overwrite"`
+	Password      string        `yaml:"password"        env:"PASSWORD, overwrite"`
 	MaxFlushDelay time.Duration `yaml:"max_flush_delay" env:"MAX_FLUSH_DELAY, overwrite, default=100μs"`
 }
 
 type Tracing struct {
 	ServiceName string  `yaml:"service_name" env:"SERVICE_NAME"`
-	Enabled     bool    `yaml:"enabled" env:"ENABLED"`
-	SampleRate  float64 `yaml:"sample_rate" env:"SAMPLE_RATE, overwrite, default=1.0"`
-	Endpoint    string  `yaml:"endpoint" env:"ENDPOINT"`
+	Enabled     bool    `yaml:"enabled"      env:"ENABLED"`
+	SampleRate  float64 `yaml:"sample_rate"  env:"SAMPLE_RATE, overwrite, default=1.0"`
+	Endpoint    string  `yaml:"endpoint"     env:"ENDPOINT"`
 }
 
 type Metrics struct {
 	Enabled bool `yaml:"enabled" env:"ENABLED"`
-	Port    int  `yaml:"port" env:"PORT, overwrite, default=8766"`
+	Port    int  `yaml:"port"    env:"PORT, overwrite, default=8766"`
 }
 
 type Sentry struct {
 	Enabled bool   `yaml:"enabled" env:"ENABLED"`
-	Dsn     string `yaml:"dsn" env:"DSN"`
+	Dsn     string `yaml:"dsn"     env:"DSN"`
 }
 
 type Profilers struct {
-	CPU           bool `yaml:"cpu" env:"CPU, default=true"`
-	AllocObjects  bool `yaml:"alloc_objects" env:"ALLOC_OBJECTS, default=true"`
-	AllocSpace    bool `yaml:"alloc_space" env:"ALLOC_SPACE, default=true"`
-	InuseObjects  bool `yaml:"inuse_objects" env:"INUSE_OBJECTS"`
-	InuseSpace    bool `yaml:"inuse_space" env:"INUSE_SPACE"`
-	Goroutines    bool `yaml:"goroutines" env:"GOROUTINES, default=true"`
-	BlockCount    bool `yaml:"block_count" env:"BLOCK_COUNT"`
+	CPU           bool `yaml:"cpu"            env:"CPU, default=true"`
+	AllocObjects  bool `yaml:"alloc_objects"  env:"ALLOC_OBJECTS, default=true"`
+	AllocSpace    bool `yaml:"alloc_space"    env:"ALLOC_SPACE, default=true"`
+	InuseObjects  bool `yaml:"inuse_objects"  env:"INUSE_OBJECTS"`
+	InuseSpace    bool `yaml:"inuse_space"    env:"INUSE_SPACE"`
+	Goroutines    bool `yaml:"goroutines"     env:"GOROUTINES, default=true"`
+	BlockCount    bool `yaml:"block_count"    env:"BLOCK_COUNT"`
 	BlockDuration bool `yaml:"block_duration" env:"BLOCK_DURATION"`
-	MutexCount    bool `yaml:"mutex_count" env:"MUTEX_COUNT"`
+	MutexCount    bool `yaml:"mutex_count"    env:"MUTEX_COUNT"`
 	MutexDuration bool `yaml:"mutex_duration" env:"MUTEX_DURATION"`
 }
 
@@ -103,17 +104,17 @@ func (p Profilers) PyroscopeTypes() []pyroscope.ProfileType {
 }
 
 type Profiling struct {
-	Enabled     bool   `yaml:"enabled" env:"ENABLED"`
+	Enabled     bool   `yaml:"enabled"      env:"ENABLED"`
 	ServiceName string `yaml:"service_name" env:"SERVICE_NAME"`
-	Endpoint    string `yaml:"endpoint" env:"ENDPOINT"`
+	Endpoint    string `yaml:"endpoint"     env:"ENDPOINT"`
 
 	Profilers Profilers `yaml:"profilers" env:", prefix=PROFILERS_"`
 }
 
 type Telemetry struct {
-	Tracing   Tracing   `yaml:"tracing" env:", prefix=TRACING_"`
-	Metrics   Metrics   `yaml:"metrics" env:", prefix=METRICS_"`
-	Sentry    Sentry    `yaml:"sentry" env:", prefix=SENTRY_"`
+	Tracing   Tracing   `yaml:"tracing"   env:", prefix=TRACING_"`
+	Metrics   Metrics   `yaml:"metrics"   env:", prefix=METRICS_"`
+	Sentry    Sentry    `yaml:"sentry"    env:", prefix=SENTRY_"`
 	Profiling Profiling `yaml:"profiling" env:", prefix=PROFILING_"`
 }
 
@@ -127,7 +128,7 @@ type Http struct {
 
 type Storage struct {
 	Enabled bool           `yaml:"enabled" env:"ENABLED, default=true"`
-	Type    string         `yaml:"type" env:"TYPE"`
+	Type    string         `yaml:"type"    env:"TYPE"`
 	Config  map[string]any `yaml:"config"`
 }
 
@@ -136,20 +137,20 @@ type Queue struct {
 }
 
 type Config struct {
-	Name        string `yaml:"name" env:"APP_NAME"`
+	Name        string `yaml:"name"        env:"APP_NAME"`
 	Environment string `yaml:"environment" env:"APP_ENV, overwrite, default=dev"`
 
 	Storage Storage `yaml:"storage" env:", prefix=STORAGE_"`
 
 	EncryptionKey string `yaml:"encryption_key" env:"ENCRYPTION_KEY"`
-	JwtSecret     string `yaml:"jwt_secret" env:"JWT_SECRET"`
+	JwtSecret     string `yaml:"jwt_secret"     env:"JWT_SECRET"`
 
 	LogLevel LogLevel `yaml:"log_level" env:"LOG_LEVEL, overwrite, default=error"`
-	Database Postgres `yaml:"database" env:", prefix=DB_"`
-	Redis    Redis    `yaml:"redis" env:", prefix=REDIS_"`
+	Database Postgres `yaml:"database"  env:", prefix=DB_"`
+	Redis    Redis    `yaml:"redis"     env:", prefix=REDIS_"`
 
 	Probes Probes `yaml:"probes" env:", prefix=PROBES_"`
-	Http   Http   `yaml:"http" env:", prefix=HTTP_"`
+	Http   Http   `yaml:"http"   env:", prefix=HTTP_"`
 
 	Telemetry Telemetry `yaml:"telemetry" env:", prefix=TELEMETRY_"`
 
--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -3,26 +3,34 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/XSAM/otelsql"
 	"github.com/henrywhitaker3/go-template/internal/config"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func Open(ctx context.Context, url string, tracing config.Tracing) (*sql.DB, error) {
+func Open(ctx context.Context, conf config.Postgres, tracing config.Tracing) (*sql.DB, error) {
+	if conf.CaCertFile != "" {
+		if !strings.Contains(conf.Url, "?") {
+			conf.Url += "&"
+		}
+		conf.Url = fmt.Sprintf("%ssslrootcert=%s", conf.Url, conf.CaCertFile)
+	}
 	var db *sql.DB
 	var err error
 	if tracing.Enabled {
 		db, err = otelsql.Open(
 			"pgx",
-			url,
+			conf.Url,
 			otelsql.WithSpanOptions(otelsql.SpanOptions{
 				OmitConnResetSession: true,
 				OmitConnectorConnect: true,
 			}),
 		)
 	} else {
-		db, err = sql.Open("pgx", url)
+		db, err = sql.Open("pgx", conf.Url)
 	}
 
 	if err != nil {
